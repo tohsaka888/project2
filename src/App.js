bf7ab94bfc624789364ff2dcaeaf8cb72010ab7f
tohsaka888 <156132264@qq.com>
2021-02-19 18:36:41 +0800

    none

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React from 'react';
+import React from "react";
 import Pc from "./Pc";
 import Mobile from "./Mobile";
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
+  const width = window.innerWidth;
+  const breakpoint = 620;
 
-    const width = window.innerWidth;
-    const breakpoint = 620;
-
-    return (
-        <Router>
-            {width < breakpoint ? <Mobile/> : <Pc/>}
-        </Router>
-    );
+  return <Router>{width < breakpoint ? <Mobile /> : <Pc />}</Router>;
 };
 
 export default App;
